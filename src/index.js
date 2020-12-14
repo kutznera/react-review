671b215fd8cfcaeb202d146758e6e0741bdd7765
kutznera <kutznera15@gmail.com>
2020-12-13 16:47:39 -0800

    Initial

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const user = {
+  firstName: 'Ana',
+  lastName: 'Kutzner',
+};
+
+const element = <h1>Hello, {formatName(user)}!</h1>;
+
+ReactDOM.render(element, document.getElementById('root'));
